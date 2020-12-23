fb668ed30c479fb726a37c5642b7f6d84061f7ee
andrey-dum <duminskiy@gmail.com>
2020-12-23 13:52:33 +0200

    Search. changeHandler. onSubmit

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,26 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { AlertContext } from '../context/alert/alertContext'
 
 const Search = () => {
     const { show } = useContext(AlertContext)
+    const [value, setValue] = useState('')
 
     const onSubmit = (e) => {
-        if (e.key === 'Enter') {
-            show('Alert')
+        if (e.key !== 'Enter') {
+            return
         }
+
+        if (value.trim()) {
+            console.log(value);
+            setValue('')
+        } else {
+            show('Введите поисковый запрос!', 'danger')
+        }
+        
+    }
+
+    const changeHandler = (e) => {
+        setValue(e.target.value)
     }
 
     return (
@@ -17,6 +30,8 @@ const Search = () => {
                 className="form-control"
                 placeholder="Enter user name"
                 onKeyPress={onSubmit}
+                value={value}
+                onChange={changeHandler}
             />
             
         </div>
